4defa652c8ce261a14e220d6cdc5f7e9d3c09473
Gabriel Sanches <gabriel@gsr.dev>
2022-02-02 20:18:40 -0300

    Deploy to pages branch (again)

--- common/deploy.sh
+++ common/deploy.sh
@@ -2,11 +2,11 @@ case "$ARCH" in
     *musl* ) libc="musl" ;;
     * ) libc="glibc" ;;
 esac
-mkdir -p "public/$libc"
+mkdir -p "$libc"
 
 # Delete files related to the package in the current architecture.
 for pkg in $(cat "$DELETED_PATH"); do
-	rm --force "public/$libc/$pkg"-[0-9]*.[0-9]*.[0-9]*_[0-9]*."$ARCH".*
+	rm --force "$libc/$pkg"-[0-9]*.[0-9]*.[0-9]*_[0-9]*."$ARCH".*
 done
 
 pkg=$(cat $ADDED_PATH $MODIFIED_PATH)
@@ -17,7 +17,7 @@ if [ "$pkg" == "" ]; then
 fi
 
 binpkgs="$UPSTREAM_PATH/hostdir/binpkgs"
-cp --recursive --force "$binpkgs"/* "public/$libc"
+cp --recursive --force "$binpkgs"/* "$libc"
 
 # Sign packages.
 if [ "$PRIVATE_PEM" == "" ]; then
@@ -27,21 +27,21 @@ fi
 
 echo "$PRIVATE_PEM" | base64 --decode > /tmp/privkey
 
-xbps-rindex --add --force "public/$libc"/*."$ARCH".xbps || exit 1
-xbps-rindex --privkey /tmp/privkey --sign --signedby "$GITLAB_USER_NAME" "public/$libc" || exit 1
+xbps-rindex --add --force "$libc"/*."$ARCH".xbps || exit 1
+xbps-rindex --privkey /tmp/privkey --sign --signedby "$GITLAB_USER_NAME" "$libc" || exit 1
 for pkg in $pkgs; do
-	xbps-rindex --privkey /tmp/privkey --sign-pkg --force "public/$libc"/pkg*."$ARCH".xbps || exit 1
+	xbps-rindex --privkey /tmp/privkey --sign-pkg --force "$libc"/pkg*."$ARCH".xbps || exit 1
 done
 
 # Generate HTML.
-cat << EOF > public/index.html
+cat << EOF > index.html
 <html>
 <head><title>$GITLAB_USER_NAME's custom Void packages</title></head>
 <body>
 <h1>Available C libraries</h1>
 <ul>
 EOF
-for lib in public/*; do
+for lib in *; do
 	path=$(basename $lib)
 
 	if [ "$path" == "index.html" ]; then
@@ -50,22 +50,22 @@ for lib in public/*; do
 
 	last_update=$(stat -c %y "$lib")
 
-	printf '<li><a href="%s">%s</a> [%s]</li>' "$path" "$path" "$last_update" >> public/index.html
+	printf '<li><a href="%s">%s</a> [%s]</li>' "$path" "$path" "$last_update" >> index.html
 done
-cat << EOF >> public/index.html
+cat << EOF >> index.html
 </ul>
 </body>
 </html>
 EOF
 
-cat << EOF > "public/$libc/index.html"
+cat << EOF > "$libc/index.html"
 <html>
 <head><title>$GITLAB_USER_NAME's custom Void packages - $libc</title></head>
 <body>
 <h1>Available packages for $libc</h1>
 <ul>
 EOF
-for file in "public/$libc"/*.xbps; do
+for file in "$libc"/*.xbps; do
 	path=$(basename $file)
 	if [ "$path" == "index.html" ]; then
 		continue
@@ -74,10 +74,41 @@ for file in "public/$libc"/*.xbps; do
 	last_update=$(stat -c %y "$file")
 	sig_file="$path.sig"
 
-	printf '<li><a href="%s">%s</a> [%s] (<a href="%s">signature</a>)</li>' "$path" "$path" "$last_update" "$sig_file" >> "public/$libc/index.html"
+	printf '<li><a href="%s">%s</a> [%s] (<a href="%s">signature</a>)</li>' "$path" "$path" "$last_update" "$sig_file" >> "$libc/index.html"
 done
-cat << EOF >> "public/$libc/index.html"
+cat << EOF >> "$libc/index.html"
 </ul>
 </body>
 </html>
 EOF
+
+# Commit changes.
+added_list=$(cat "$ADDED_PATH" | sed --regexp-extended "s/(.+)/  * \1/")
+modified_list=$(cat "$MODIFIED_PATH" | sed --regexp-extended "s/(.+)/  * \1/")
+deleted_list=$(cat "$DELETED_PATH" | sed --regexp-extended "s/(.+)/  * \1/")
+
+added_list=${added_list:-"  (Nothing)"}
+modified_list=${modified_list:-"  (Nothing)"}
+deleted_list=${deleted_list:-"  (Nothing)"}
+
+git config --global user.name "GitLab CI (job #$CI_JOB_ID)"
+git config --global user.email "$GITLAB_USER_EMAIL"
+git add index.html $libc
+git commit --file - << EOF
+Update packages for $libc
+
+Added packages:
+$added_list
+
+Updated packages:
+$modified_list
+
+Deleted packages:
+$deleted_list
+EOF
+
+git remote set-url origin "https://${GITLAB_USER_LOGIN}:${ACCESS_TOKEN}@gitlab.com/${CI_PROJECT_PATH}.git"
+
+echo "Pushing to $(git remote get-url origin)..."
+git push --set-upstream --force --quiet origin $repo_branch || exit 1
+echo "Done!!!"
