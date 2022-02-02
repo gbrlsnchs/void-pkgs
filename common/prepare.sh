bbad628e66b403cabccea44dabed09b486ec3320
Gabriel Sanches <gabriel@gsr.dev>
2022-02-02 09:07:44 -0300

    Fix formatting of eligible packages

--- common/prepare.sh
+++ common/prepare.sh
@@ -37,7 +37,7 @@ fi
 # building other packages.
 eligible_pkgs=$(ls -1 srcpkgs | grep --invert-match --file /tmp/removed)
 echo "The following packages will be added/updated:"
-echo $eligible_pkgs | sed "s/^/ *  /"
+echo $eligible_pkgs | sed "s/^/  * /"
 for pkg in $eligible_pkgs; do
 	cp --no-target-directory --recursive srcpkgs/$pkg $dir/$pkg
 done
