f4afc87615d74f629937fe2040cb4ce8f2ab158a
Gabriel Sanches <gabriel@gsr.dev>
2022-03-06 15:26:15 -0300

    Remove existing repodata before signing

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -8,12 +8,17 @@ echo "$SIGNING_KEY" > /tmp/signing_key
 
 # Delete files related to the package in the current architecture.
 for libc in */; do
+	# Clean obsolete packages and existing repodata.
 	for pkg in $(cat /tmp/ci/modified /tmp/ci/deleted); do
-		rm --force "$libc/$pkg"-[0-9]*.[0-9]*.[0-9]*_[0-9]*."$ARCH".*
+		rm --force "$libc/$pkg"-[0-9]*.[0-9]*.[0-9]*_[0-9]*.*.*
 	done
+	rm --force "$libc"/*-repodata
 
 	cp --force /tmp/pre/"$libc"/* "$libc"
+	echo "All files in $libc:"
+	find "$libc" -maxdepth 1 -path "$libc/*" -printf "%f\n" | sed "s/^/  * /"
 
+	# Sign the repository and its packages.
 	xbps-rindex --add "$libc"/*.xbps || exit 1
 	xbps-rindex --privkey /tmp/signing_key --sign --signedby "$CI_COMMIT_AUTHOR" "$libc" || exit 1
 	xbps-rindex --privkey /tmp/signing_key --sign-pkg "$libc"/*.xbps || exit 1
