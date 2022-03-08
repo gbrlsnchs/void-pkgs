c66a18b94a20124f13f6cbc7b515725c8117300d
Gabriel Sanches <gabriel@gsr.dev>
2022-03-08 17:20:50 -0300

    Narrow down packages signed by each iteration

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -22,13 +22,15 @@ for libc in *; do
 	find "$libc" -maxdepth 1 -path "$libc/*" -printf "%f\n" | sed "s/^/  * /"
 
 	for repodata in "$libc"/*-repodata; do
-		export XBPS_TARGET_ARCH="${repodata%-repodata}"
+		arch="${repodata%-repodata}"
+		export XBPS_TARGET_ARCH="$arch"
 
 		# Sign the repository and its packages.
-		xbps-rindex --add "$libc"/*.xbps || exit 1
-		xbps-rindex --privkey /tmp/signing_key --sign --signedby "$CI_COMMIT_AUTHOR" "$libc" || exit 1
-		xbps-rindex --privkey /tmp/signing_key --sign-pkg "$libc"/*.xbps || exit 1
+		xbps-rindex --add "$libc"/*."$arch".xbps || exit 1
+		xbps-rindex --privkey /tmp/signing_key --sign-pkg "$libc"/*."$arch".xbps || exit 1
 
 		unset XBPS_TARGET_ARCH
 	done
+
+	xbps-rindex --privkey /tmp/signing_key --sign --signedby "$CI_COMMIT_AUTHOR" "$libc" || exit 1
 done
