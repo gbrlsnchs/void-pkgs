bc1fdc9d401c13cf856a1c9aab7a48a92a1ebbde
Gabriel Sanches <gabriel@gsr.dev>
2022-03-08 17:17:43 -0300

    Set target arch based on repodata before signing

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -16,14 +16,19 @@ for libc in *; do
 	for pkg in $(cat /tmp/ci/modified /tmp/ci/deleted); do
 		rm --force "$libc/$pkg"-[0-9]*.[0-9]*.[0-9]*_[0-9]*.*.*
 	done
-	rm --force "$libc"/*-repodata
 
 	cp --force /tmp/pre/"$libc"/* "$libc"
 	echo "All files in $libc:"
 	find "$libc" -maxdepth 1 -path "$libc/*" -printf "%f\n" | sed "s/^/  * /"
 
-	# Sign the repository and its packages.
-	xbps-rindex --add "$libc"/*.xbps || exit 1
-	xbps-rindex --privkey /tmp/signing_key --sign --signedby "$CI_COMMIT_AUTHOR" "$libc" || exit 1
-	xbps-rindex --privkey /tmp/signing_key --sign-pkg "$libc"/*.xbps || exit 1
+	for repodata in "$libc"/*-repodata; do
+		export XBPS_TARGET_ARCH="${repodata%-repodata}"
+
+		# Sign the repository and its packages.
+		xbps-rindex --add "$libc"/*.xbps || exit 1
+		xbps-rindex --privkey /tmp/signing_key --sign --signedby "$CI_COMMIT_AUTHOR" "$libc" || exit 1
+		xbps-rindex --privkey /tmp/signing_key --sign-pkg "$libc"/*.xbps || exit 1
+
+		unset XBPS_TARGET_ARCH
+	done
 done
