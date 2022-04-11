ec7bd72a1d76a8bed626095bff65d586fdae3e2a
Gabriel Sanches <gabriel@gsr.dev>
2022-04-11 18:34:19 -0300

    Try removing pages worktree before trimming

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -12,6 +12,7 @@ git switch ci
 git reset --hard tmp_ci
 git push --force --set-upstream origin ci
 
+git worktree remove pages
 git switch --orphan tmp_pages
 git fetch origin pages:pages
 git cherry-pick pages
