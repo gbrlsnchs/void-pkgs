c8a431dda9c11496b03163df48e99055c48c8a95
Gabriel Sanches <gabriel@gsr.dev>
2022-05-18 18:18:59 -0300

    Remove all signatures and repodata before signing

--- scripts/xbps/rindex.sh
+++ scripts/xbps/rindex.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm --force *.xbps.sig *-repodata
 xbps-rindex --add *.xbps || exit 1
 xbps-rindex --sign --privkey /var/private.pem --signedby "Gabriel Sanches" . || exit 1
 xbps-rindex --sign-pkg --privkey /var/private.pem *.xbps || exit 1
