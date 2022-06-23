386b02a00f03a7c47a22bb94496c13106659640a
Gabriel Sanches <gabriel@gsr.dev>
2022-06-23 16:46:07 -0300

    Fix source code linking

--- scripts/local/html.sh
+++ scripts/local/html.sh
@@ -8,7 +8,12 @@ get_last_update() {
 	git --no-pager log -1 --format="%ad" -- "$1"
 }
 
-repo_url="$(git config --get remote.origin.url | sed --expression "s/git@\(.*\)\.git/\1/")"
+repo_url="$(
+	git config --get remote.origin.url | \
+	sed \
+		--expression "s/git@\(.*\)\.git/\1/" \
+		--expression "s|:|/|"
+)"
 repo_branch="$(git rev-parse --abbrev-ref HEAD)"
 
 author="Gabriel Sanches"
@@ -42,14 +47,15 @@ for libc in *; do
 		pkgname="$(basename "$pkgbin")"
 		pkgsize="$(get_size "$pkgbin")"
 		last_update="$(get_last_update "$pkgbin")"
-		last_update_sig=$(get_last_update "$pkgbin.sig")
+		last_update_sig="$(get_last_update "$pkgbin.sig")"
+		original_pkgname="$(echo "$pkgname" | sed "s/\(.*\)-[0-9]*\.[0-9]*\.[0-9]*_[0-9]*\..*/\1/")"
 
 		entries="$(
 			sed \
 				--expression "s/{{name}}/$pkgname/g" \
 				--expression "s/{{size}}/$pkgsize/g" \
 				--expression "s/{{last_update}}/$last_update/g" \
-				--expression "s|{{source_base_url}}|https://$repo_url/src/branch/$repo_branch|g" \
+				--expression "s|{{source_url}}|https://$repo_url/src/branch/$repo_branch/srcpkgs/${original_pkgname}|g" \
 				../templates/pkgs.html
 		)"
 		rows="$rows\n$entries"
@@ -89,11 +95,13 @@ awk \
 	-v author="$author" \
 	-v meta="$meta" \
 	-v common_styles="$common_styles" \
+	-v repo_url="https://$repo_url" \
 	-v rows="$librows" \
 	'{
 		sub(/{{author}}/, author);
 		sub(/{{meta}}/, meta);
 		sub(/{{common_styles}}/, common_styles);
+		sub(/{{repo_url}}/, repo_url);
 		sub(/{{rows}}/, rows);
 		print;
 	}' \
