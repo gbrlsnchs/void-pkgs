7998fae75fe918eac5208ed5f50127cd4eff8088
Gabriel Sanches <gabriel@gsr.dev>
2022-07-11 11:58:00 -0300

    scripts: Update location of private key

--- scripts/local/sign.sh
+++ scripts/local/sign.sh
@@ -13,7 +13,7 @@ podman container run \
 	--env="XBPS_TARGET_ARCH=$arch" \
 	--volume="$PWD/void-bins/$libc:/etc/void-bins" \
 	--volume="$PWD/scripts/xbps/rindex.sh:/opt/rindex.sh" \
-	--volume="$PWD/data/private.pem:/var/private.pem" \
+	--volume="$PWD/private.pem:/var/private.pem" \
 	--workdir=/etc/void-bins \
 	"ghcr.io/void-linux/xbps-src-masterdir:20210313rc01-$bootstrap_arch" \
 	/opt/rindex.sh
